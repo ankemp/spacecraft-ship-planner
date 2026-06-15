7a52b32e70eed996793dc63c8f44945eb0ac523b
Andrew Kemp <andrew@ankemp.com>
2026-06-15 15:12:00 -0500

    update base path for GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.GITHUB_ACTIONS ? '/spacecraft-shipbuilder/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/spacecraft-ship-planner/' : '/',
 })
